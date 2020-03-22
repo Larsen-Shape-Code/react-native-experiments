3f72538f6e33b662052aa6db10481f3942e2511d
Larsen Marquardt <Larsen.Marquardt@gmail.com>
2020-03-21 21:46:32 -0400

    Todo list app works

--- project0/script.js
+++ project0/script.js
@@ -12,27 +12,35 @@ const uncheckedCountSpan = document.getElementById('unchecked-count')
 function newTodo() {
   itemCountSpan.innerHTML++;
   uncheckedCountSpan.innerHTML++;
+
   let listItem = document.createElement("li");
-
   let todoTextInput = document.createElement("input");
-
   let checkbox = document.createElement("input");
+  let deleteButton = document.createElement("button");
+
   checkbox.setAttribute("type", "checkbox");
   checkbox.setAttribute("onchange", "checkTodo(this)");
-
   checkbox.setAttribute("class", classNames.TODO_CHECKBOX);
+  deleteButton.setAttribute("onclick", "deleteTodo(this.parentNode.id)");
+  deleteButton.innerHTML = "Delete";
+
   todoTextInput.setAttribute("class", classNames.TODO_TEXT);
   
   listItem.appendChild(todoTextInput);
   listItem.appendChild(checkbox);
+  listItem.appendChild(deleteButton);
   listItem.setAttribute("class", classNames.TODO_ITEM);
-  list.appendChild(listItem); 
+  listItem.setAttribute("id", (itemCountSpan.innerHTML));
+  list.appendChild(listItem);
 }
 
 function checkTodo(checkbox) {
   return (checkbox.checked ? uncheckedCountSpan.innerHTML-- : uncheckedCountSpan.innerHTML++);
 }
 
-function deleteTodo() {
+function deleteTodo(parentId) {
   itemCountSpan.innerHTML--;
+  uncheckedCountSpan.innerHTML--;
+  let itemToDelete = document.getElementById(parentId);
+  itemToDelete.remove();
 }
